15c1d0e7244b1f9147675b20a38c2851e9f191a2
cjerotich <97061509+cjerotich@users.noreply.github.com>
2022-01-21 04:01:01 -0500

    Add files via upload

---
+++ block_letters.py
@@ -0,0 +1,10 @@
+#Carol Jerotich
+#Block Letters Project
+
+print(" CCC     JJJJJJ ")
+print("C    C      J   ")
+print("C           J  ")
+print("C           J  ")
+print("C        J  J  ")
+print("C    C   J  J  ")
+print(" CCC       JJ  ")
